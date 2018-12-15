eeef7530dda12149e49a54cfd540cf7f413e586f
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-12-15 22:13:25 +0900

    init

--- pbmodels/download_tiny-yolov3.sh
+++ pbmodels/download_tiny-yolov3.sh
@@ -1,6 +1,6 @@
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1Rpy7jPrxRm4NuJdJQHL3nqFjM0TuQQMJ" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1Slnh3ShPOog1En5rkWi5NUQaVhRp7M2M" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1Rpy7jPrxRm4NuJdJQHL3nqFjM0TuQQMJ" -o tiny-YoloV3.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1Slnh3ShPOog1En5rkWi5NUQaVhRp7M2M" -o tiny-YoloV3.tar.gz
 tar -zxvf tiny-YoloV3.tar.gz
 rm tiny-YoloV3.tar.gz
 
