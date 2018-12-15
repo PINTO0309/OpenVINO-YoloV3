c93769c292ec1bd7737d8bb1d0b8d80070c6c6b9
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-12-15 22:17:50 +0900

    init

--- lrmodels/YoloV3/FP16/download_yolov3lrFP16.sh
+++ lrmodels/YoloV3/FP16/download_yolov3lrFP16.sh
@@ -1,6 +1,6 @@
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1bxetLBM3cd7t4oOeSZKrMWWkJJgsooXs" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1Kbw_YEsoc3hCRN7jNsB6JI3bMKrPeXkB" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1bxetLBM3cd7t4oOeSZKrMWWkJJgsooXs" -o YoloV3FP16.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1Kbw_YEsoc3hCRN7jNsB6JI3bMKrPeXkB" -o YoloV3FP16.tar.gz
 tar -zxvf YoloV3FP16.tar.gz
 rm YoloV3FP16.tar.gz
 
--- lrmodels/YoloV3/FP32/download_yolov3lrFP32.sh
+++ lrmodels/YoloV3/FP32/download_yolov3lrFP32.sh
@@ -1,6 +1,6 @@
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1uTXsPJGM5zMGU5OnCJl6d2yft7yzRP3c" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1s-KXvvAfsuMu8X8mbK57pYCo81zI5jDi" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1uTXsPJGM5zMGU5OnCJl6d2yft7yzRP3c" -o YoloV3FP32.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1s-KXvvAfsuMu8X8mbK57pYCo81zI5jDi" -o YoloV3FP32.tar.gz
 tar -zxvf YoloV3FP32.tar.gz
 rm YoloV3FP32.tar.gz
 
