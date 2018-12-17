a3baecac902b9d5df3967eda814866e7e08b7556
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-12-17 10:20:53 +0900

    init

---
+++ weights/download_yolov3_weights.sh
@@ -0,0 +1,6 @@
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=17rhAPwZOW9x_dL3vL5Fo1SsIJlnKYxHh" > /dev/null
+CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=17rhAPwZOW9x_dL3vL5Fo1SsIJlnKYxHh" -o Yolo3.tar.gz
+tar -zxvf Yolo3.tar.gz
+rm Yolo3.tar.gz
+
