13c851e6556bb0ae697b6d4bf087b27f38e31ba6
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-12-23 17:40:44 +0900

    update

--- lrmodels/tiny-YoloV3/FP16/download_tiny-yolov3lrFP16.sh
+++ lrmodels/tiny-YoloV3/FP16/download_tiny-yolov3lrFP16.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1qRO7Mf8pS1Urno5z7ZE4U0W9CYTJnk_B" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1qYTE1fEwSduDYrv6f3J_3GKbkio5m3E2" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1qRO7Mf8pS1Urno5z7ZE4U0W9CYTJnk_B" -o tiny-YoloV3FP16.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1qYTE1fEwSduDYrv6f3J_3GKbkio5m3E2" -o tiny-YoloV3FP16.tar.gz
 tar -zxvf tiny-YoloV3FP16.tar.gz
 rm tiny-YoloV3FP16.tar.gz
 
--- lrmodels/tiny-YoloV3/FP32/download_tiny-yolov3lrFP32.sh
+++ lrmodels/tiny-YoloV3/FP32/download_tiny-yolov3lrFP32.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=15SfP7JBhke35AG4KZwkyJVEzy56_A-PH" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1syT67dXRnCNeGYbYkLHy4p7TpAqow2Js" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=15SfP7JBhke35AG4KZwkyJVEzy56_A-PH" -o tiny-YoloV3FP32.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1syT67dXRnCNeGYbYkLHy4p7TpAqow2Js" -o tiny-YoloV3FP32.tar.gz
 tar -zxvf tiny-YoloV3FP32.tar.gz
 rm tiny-YoloV3FP32.tar.gz
 
