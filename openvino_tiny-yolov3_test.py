747ea9532b30265d97f37c479f9e274ae5e6940a
PINTO0309 <rmsdh122@yahoo.co.jp>
2018-12-28 16:27:19 +0900

    tiny-YoloV3's Python support

--- openvino_tiny-yolov3_test.py
+++ openvino_tiny-yolov3_test.py
@@ -1,28 +1,18 @@
-import sys
-import os
-from argparse import ArgumentParser
+import sys, os, cv2, time
 import numpy as np, math
-import cv2
-import time
-from PIL import Image
-import tensorflow as tf
-from tensorflow.python.platform import gfile
+from argparse import ArgumentParser
 from openvino.inference_engine import IENetwork, IEPlugin
 
 m_input_size = 416
 
 yolo_scale_13 = 13
 yolo_scale_26 = 26
+yolo_scale_52 = 52
 
 classes = 80
 coords = 4
-num = 6
-mask = [0,1,2]
-jitter = 0.3
-ignore_thresh = 0.7
-truth_thresh = 1
-random = 1
-anchors = {10,14, 23,27, 37,58, 81,82, 135,169, 344,319}
+num = 3
+anchors = [10,14, 23,27, 37,58, 81,82, 135,169, 344,319]
 
 LABELS = ("person", "bicycle", "car", "motorbike", "aeroplane",
           "bus", "train", "truck", "boat", "traffic light",
@@ -41,66 +31,109 @@ LABELS = ("person", "bicycle", "car", "motorbike", "aeroplane",
           "toaster", "sink", "refrigerator", "book", "clock",
           "vase", "scissors", "teddy bear", "hair drier", "toothbrush")
 
+label_text_color = (255, 255, 255)
+label_background_color = (125, 175, 75)
+box_color = (255, 128, 0)
+box_thickness = 1
+
 def build_argparser():
     parser = ArgumentParser()
     parser.add_argument("-d", "--device", help="Specify the target device to infer on; CPU, GPU, FPGA or MYRIAD is acceptable. \
                                                 Sample will look for a suitable plugin for device specified (CPU by default)", default="CPU", type=str)
     return parser
 
-#def ParseYOLOV3Output(blob, resized_im_h, resized_im_w, original_im_h, original_im_w, threshold, objects):
-#    ## --------------------------- Validating output parameters -------------------------------------
-#    #const int out_blob_h = static_cast<int>(blob->getTensorDesc().getDims()[2]);
-#    #const int out_blob_w = static_cast<int>(blob->getTensorDesc().getDims()[3]);
-#    ## --------------------------- Extracting layer parameters -------------------------------------
-#    side = out_blob_h
-#    anchor_offset = 0
-#
-#    if anchors.size() == 18      ## YoloV3
-#        if side == yolo_scale_13:
-#            anchor_offset = 2 * 6
-#        else if side == yolo_scale_26:
-#            anchor_offset = 2 * 3
-#        else if side == yolo_scale_52:
-#            anchor_offset = 2 * 0
-#
-#    else if anchors.size() == 12 ## tiny-YoloV3
-#        if side == yolo_scale_13:
-#            anchor_offset = 2 * 3
-#        else if side == yolo_scale_26:
-#            anchor_offset = 2 * 0
-#
-#    else                         ## ???
-#        if side == yolo_scale_13:
-#            anchor_offset = 2 * 6
-#        else if side == yolo_scale_26:
-#            anchor_offset = 2 * 3
-#        else if side == yolo_scale_52:
-#            anchor_offset = 2 * 0
-#
-#    side_square = side * side
-#    const float *output_blob = blob->buffer().as<PrecisionTrait<Precision::FP32>::value_type *>();
-#
-#    ## --------------------------- Parsing YOLO Region output -------------------------------------
-#    for i in range(side_square):
-#        row = int(i / side)
-#        col = int(i % side)
-#        for n in range(num):
-#            obj_index = EntryIndex(side, coords, classes, n * side * side + i, coords)
-#            box_index = EntryIndex(side, coords, classes, n * side * side + i, 0)
-#            scale = output_blob[obj_index]
-#            if (scale < threshold):
-#                continue
-#            x = (col + output_blob[box_index + 0 * side_square]) / side * resized_im_w
-#            y = (row + output_blob[box_index + 1 * side_square]) / side * resized_im_h
-#            height = math.exp(output_blob[box_index + 3 * side_square]) * anchors[anchor_offset + 2 * n + 1]
-#            width = math.exp(output_blob[box_index + 2 * side_square]) * anchors[anchor_offset + 2 * n]
-#            for j in range(classes):
-#                class_index = EntryIndex(side, coords, classes, n * side_square + i, coords + 1 + j)
-#                prob = scale * output_blob[class_index]
-#                if prob < threshold:
-#                    continue
-#                DetectionObject obj(x, y, height, width, j, prob, (original_im_h / resized_im_h), (original_im_w / resized_im_w))
-#                objects.push_back(obj)
+
+def EntryIndex(side, lcoords, lclasses, location, entry):
+    n = int(location / (side * side))
+    loc = location % (side * side)
+    return int(n * side * side * (lcoords + lclasses + 1) + entry * side * side + loc)
+
+
+class DetectionObject():
+    xmin = 0
+    ymin = 0
+    xmax = 0
+    ymax = 0
+    class_id = 0
+    confidence = 0.0
+
+    def __init__(self, x, y, h, w, class_id, confidence, h_scale, w_scale):
+        self.xmin = int((x - w / 2) * w_scale)
+        self.ymin = int((y - h / 2) * h_scale)
+        self.xmax = int(self.xmin + w * w_scale)
+        self.ymax = int(self.ymin + h * h_scale)
+        self.class_id = class_id
+        self.confidence = confidence
+
+
+def IntersectionOverUnion(box_1, box_2):
+    width_of_overlap_area = min(box_1.xmax, box_2.xmax) - max(box_1.xmin, box_2.xmin)
+    height_of_overlap_area = min(box_1.ymax, box_2.ymax) - max(box_1.ymin, box_2.ymin)
+    area_of_overlap = 0.0
+    if (width_of_overlap_area < 0.0 or height_of_overlap_area < 0.0):
+        area_of_overlap = 0.0
+    else:
+        area_of_overlap = width_of_overlap_area * height_of_overlap_area
+    box_1_area = (box_1.ymax - box_1.ymin)  * (box_1.xmax - box_1.xmin)
+    box_2_area = (box_2.ymax - box_2.ymin)  * (box_2.xmax - box_2.xmin)
+    area_of_union = box_1_area + box_2_area - area_of_overlap
+    return (area_of_overlap / area_of_union)
+
+
+def ParseYOLOV3Output(blob, resized_im_h, resized_im_w, original_im_h, original_im_w, threshold, objects):
+
+    out_blob_h = blob.shape[2]
+    out_blob_w = blob.shape[3]
+
+    side = out_blob_h
+    anchor_offset = 0
+
+    if len(anchors) == 18:   ## YoloV3
+        if side == yolo_scale_13:
+            anchor_offset = 2 * 6
+        elif side == yolo_scale_26:
+            anchor_offset = 2 * 3
+        elif side == yolo_scale_52:
+            anchor_offset = 2 * 0
+
+    elif len(anchors) == 12: ## tiny-YoloV3
+        if side == yolo_scale_13:
+            anchor_offset = 2 * 3
+        elif side == yolo_scale_26:
+            anchor_offset = 2 * 0
+
+    else:                    ## ???
+        if side == yolo_scale_13:
+            anchor_offset = 2 * 6
+        elif side == yolo_scale_26:
+            anchor_offset = 2 * 3
+        elif side == yolo_scale_52:
+            anchor_offset = 2 * 0
+
+    side_square = side * side
+    output_blob = blob.flatten()
+
+    for i in range(side_square):
+        row = int(i / side)
+        col = int(i % side)
+        for n in range(num):
+            obj_index = EntryIndex(side, coords, classes, n * side * side + i, coords)
+            box_index = EntryIndex(side, coords, classes, n * side * side + i, 0)
+            scale = output_blob[obj_index]
+            if (scale < threshold):
+                continue
+            x = (col + output_blob[box_index + 0 * side_square]) / side * resized_im_w
+            y = (row + output_blob[box_index + 1 * side_square]) / side * resized_im_h
+            height = math.exp(output_blob[box_index + 3 * side_square]) * anchors[anchor_offset + 2 * n + 1]
+            width = math.exp(output_blob[box_index + 2 * side_square]) * anchors[anchor_offset + 2 * n]
+            for j in range(classes):
+                class_index = EntryIndex(side, coords, classes, n * side_square + i, coords + 1 + j)
+                prob = scale * output_blob[class_index]
+                if prob < threshold:
+                    continue
+                obj = DetectionObject(x, y, height, width, j, prob, (original_im_h / resized_im_h), (original_im_w / resized_im_w))
+                objects.append(obj)
+    return objects
 
 
 def main_IE_infer():
@@ -136,7 +169,6 @@ def main_IE_infer():
     plugin = IEPlugin(device=args.device)
     if "CPU" in args.device:
         plugin.add_cpu_extension("lib/libcpu_extension.so")
-    # Read IR
     net = IENetwork(model=model_xml, weights=model_bin)
     input_blob = next(iter(net.inputs))
     exec_net = plugin.load(network=net)
@@ -151,38 +183,41 @@ def main_IE_infer():
         if not ret:
             break
 
-        image = cv2.resize(image, (m_input_size, m_input_size), interpolation=cv2.INTER_CUBIC)
-
-        prepimg = image[np.newaxis, :, :, :]
-        prepimg = prepimg.transpose((0, 3, 1, 2))  #NHWC to NCHW
+        prepimg = cv2.resize(image, (m_input_size, m_input_size))
+        prepimg = prepimg[np.newaxis, :, :, :]     # Batch size axis add
+        prepimg = prepimg.transpose((0, 3, 1, 2))  # NHWC to NCHW
         outputs = exec_net.infer(inputs={input_blob: prepimg})
 
         #output_name = detector/yolo-v3-tiny/Conv_12/BiasAdd/YoloRegion
         #output_name = detector/yolo-v3-tiny/Conv_9/BiasAdd/YoloRegion
-        print(type(outputs))
-        print(outputs["detector/yolo-v3-tiny/Conv_12/BiasAdd/YoloRegion"])
-        print(outputs["detector/yolo-v3-tiny/Conv_9/BiasAdd/YoloRegion"])
-
-        sys.exit(0)
-        break
 
         objects = []
-        # Parsing outputs
+
         for output in outputs.values():
-            ParseYOLOV3Output(output, m_input_size, m_input_size, camera_height, camera_width, 0.2, objects)
+            objects = ParseYOLOV3Output(output, m_input_size, m_input_size, camera_height, camera_width, 0.2, objects)
+
+        # Filtering overlapping boxes
+        objlen = len(objects)
+        for i in range(objlen):
+            if (objects[i].confidence == 0.0):
+                continue
+            for j in range(i + 1, objlen):
+                if (IntersectionOverUnion(objects[i], objects[j]) >= 0.4):
+                    objects[j].confidence = 0
         
+        # Drawing boxes
+        for obj in objects:
+            if obj.confidence < 0.2:
+                continue
+            label = obj.class_id
+            confidence = obj.confidence
+            if confidence > 0.2:
+                label_text = LABELS[label] + " (" + "{:.1f}".format(confidence * 100) + "%)"
+                cv2.rectangle(image, (obj.xmin, obj.ymin), (obj.xmax, obj.ymax), box_color, box_thickness)
+                cv2.putText(image, label_text, (obj.xmin, obj.ymin - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, label_text_color, 1)
 
-
-        outputimg = Image.fromarray(np.uint8(result), mode="P")
-        outputimg.putpalette(palette)
-        outputimg = outputimg.convert("RGB")
-        outputimg = np.asarray(outputimg)
-        outputimg = cv2.cvtColor(outputimg, cv2.COLOR_RGB2BGR)
-        outputimg = cv2.addWeighted(image, 1.0, outputimg, 0.9, 0)
-        outputimg = cv2.resize(outputimg, (camera_width, camera_height))
-
-        cv2.putText(outputimg, fps, (camera_width-180,15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (38,0,255), 1, cv2.LINE_AA)
-        cv2.imshow("Result", outputimg)
+        cv2.putText(image, fps, (camera_width - 170, 15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (38, 0, 255), 1, cv2.LINE_AA)
+        cv2.imshow("Result", image)
 
         if cv2.waitKey(1)&0xFF == ord('q'):
             break
