df0b3b3bc84b267d7ff5cb34f818109d8d4f020d
PINTO0309 <rmsdh122@yahoo.co.jp>
2019-04-14 19:47:21 +0900

    Compatible with 2019R1

--- lrmodels/YoloV3/FP32/download_yolov3lrFP32.sh
+++ lrmodels/YoloV3/FP32/download_yolov3lrFP32.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1U-ilK5qJXZslsiz5wzeywMARKaMU8CpQ" > /dev/null
+curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=1r2hCNqPMKrbTL5D30ZoNwY5cp3PQIXb4" > /dev/null
 CODE="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
-curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1U-ilK5qJXZslsiz5wzeywMARKaMU8CpQ" -o YoloV3FP32.tar.gz
+curl -Lb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${CODE}&id=1r2hCNqPMKrbTL5D30ZoNwY5cp3PQIXb4" -o YoloV3FP32.tar.gz
 tar -zxvf YoloV3FP32.tar.gz
 rm YoloV3FP32.tar.gz
 
